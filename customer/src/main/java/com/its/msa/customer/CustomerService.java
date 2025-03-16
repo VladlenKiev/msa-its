1fdaafc610ec9173bd32e6724951dee7f66af1cb
vladlenkiev <v.b.mankivskiy@gmail.com>
2025-03-16 19:27:46 +0100

    Module 5: added load balancer; discovery clients

--- customer/src/main/java/com/its/msa/customer/CustomerService.java
+++ customer/src/main/java/com/its/msa/customer/CustomerService.java
@@ -2,6 +2,7 @@ package com.its.msa.customer;
 
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
@@ -12,14 +13,25 @@ public class CustomerService {
     private final CustomerRepository customerRepository;
     private final RestTemplate restTemplate;
 
+    private final DiscoveryClient discoveryClient;
+
+    public String getFraudServiceUrl() {
+        return discoveryClient.getInstances("FRAUD").stream()
+                .findFirst()
+                .map(si -> si.getUri().toString())
+                .orElseThrow(() -> new IllegalStateException("Fraud service not found!"));
+    }
+
     public void register(Customer customer) {
         log.info("customer: {}", customer);
         //TODO validate email is not null
         //TODO validate name is not null
         //TODO store to DB
         customerRepository.saveAndFlush(customer);
+        log.info("customer saved: {}", customer);
         //TODO isFraudster
-        FraudCheckResponse fraudCheckResponse = restTemplate.getForObject("http://localhost:8082/api/v1/fraud-check/{customerId}",
+        FraudCheckResponse fraudCheckResponse = restTemplate
+                .getForObject(getFraudServiceUrl() + "/api/v1/fraud-check/{customerId}",
                 FraudCheckResponse.class, customer.getId());
         if (fraudCheckResponse.isFraudster()) {
             throw new IllegalStateException("It's a fraudster with customerId " + customer.getId());
